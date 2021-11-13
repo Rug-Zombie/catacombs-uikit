f1eed8e54d9e863d13f43487628f1e57ddec7ac4
Nnamdi Arimah <nnamdi.arimah@shopify.com>
2021-11-13 16:05:21 -0500

    revert home icon

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -3188,7 +3188,7 @@ var Icon$q = function (props) {
 
 var Icon$p = function (props) {
     return (React.createElement(Svg, __assign({ viewBox: "0 0 355 307", xmlns: "http://www.w3.org/2000/svg" }, props),
-        React.createElement("path", { fill: "#101820", stroke: "#b5bd00", strokeMiterlimit: "10", strokeWidth: "10px", d: "M364.75,177.57h0L203.36,36.74h0a16,16,0,0,0-22.72,0h0L19.25,177.57h0A8,8,0,0,0,24,192H64V320a16,16,0,0,0,16,16h64a16,16,0,0,0,16-16V224h64v96a16,16,0,0,0,16,16h64a16,16,0,0,0,16-16V192h40a8,8,0,0,0,4.75-14.43Z", transform: "translate(-14.5 -30.5)" })));
+        React.createElement("path", { fill: "white", strokeMiterlimit: "10", strokeWidth: "10px", d: "M364.75,177.57h0L203.36,36.74h0a16,16,0,0,0-22.72,0h0L19.25,177.57h0A8,8,0,0,0,24,192H64V320a16,16,0,0,0,16,16h64a16,16,0,0,0,16-16V224h64v96a16,16,0,0,0,16,16h64a16,16,0,0,0,16-16V192h40a8,8,0,0,0,4.75-14.43Z", transform: "translate(-14.5 -30.5)" })));
 };
 
 var Icon$o = function (props) {
