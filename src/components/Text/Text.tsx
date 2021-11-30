174ad9de31c6db9a3812152a52c846d79ee835ca
saadSarwar28 <saad_sarwar22@hotmail.com>
2021-12-01 03:04:04 +0500

    fix(modal): text color changed

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -16,7 +16,7 @@ const getFontSize = ({ fontSize, small }: TextProps) => {
 };
 
 const Text = styled.div<TextProps>`
-  color: ${getColor};
+  color: white;
   font-size: ${getFontSize};
   font-weight: ${({ bold }) => (bold ? 600 : 400)};
   line-height: 1.5;
